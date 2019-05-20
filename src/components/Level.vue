29a8b1ed522effff0cfdf7eff74ee60e835dbb46
delylaric <delylaric@gmail.com>
2019-05-20 16:32:13 +0800

    stash

--- src/components/Level.vue
+++ src/components/Level.vue
@@ -1,35 +1,27 @@
 <template>
-  <nav class="level">
-    <!-- Left side -->
-    <div class="level-left">
-      <div class="level-item">
-        <p class="subtitle is-5">
-          <strong>123</strong> posts
-        </p>
-      </div>
-      <div class="level-item">
-        <div class="field has-addons">
-          <p class="control">
-            <input class="input" type="text" placeholder="Find a post">
-          </p>
-          <p class="control">
-            <button class="button">
-              Search
-            </button>
-          </p>
+  <div class="app-level-container">
+    <div class="container">
+    <nav class="level">
+      <!-- Left side -->
+      <div class="level-left">
+        <div class="level-item">
+          <router-link to="/" class="app-logo title">
+            LOGO
+          </router-link>
         </div>
       </div>
-    </div>
 
-    <!-- Right side -->
-    <div class="level-right">
-      <p class="level-item"><strong>All</strong></p>
-      <p class="level-item"><a>Published</a></p>
-      <p class="level-item"><a>Drafts</a></p>
-      <p class="level-item"><a>Deleted</a></p>
-      <p class="level-item"><a class="button is-success">New</a></p>
+      <!-- Right side -->
+      <div class="level-right">
+        <p class="level-item"><strong>All</strong></p>
+        <p class="level-item"><a>Published</a></p>
+        <p class="level-item"><a>Drafts</a></p>
+        <p class="level-item"><a>Deleted</a></p>
+        <p class="level-item"><a class="button is-success">New</a></p>
+      </div>
+    </nav>
     </div>
-  </nav>
+  </div>
 </template>
 
 <script>
@@ -37,3 +29,17 @@ export default {
   name: 'Level'
 }
 </script>
+
+<style lang="sass">
+
+.app-level-container
+  min-height: 5rem
+  z-index: 10000
+  background-color: #FFFFFF
+  box-shadow: 0 3px 3px rgba(0,0,0,0.1)
+  .level
+    height: 5rem
+
+.app-logo
+  cursor: pointer
+</style>
